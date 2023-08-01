971e4dae10bffe252f59dd3596dd27d03e887e71
xero <letsdoit3691@gmail.com>
2023-08-01 18:10:01 +0200

    semi final commandline gui added

---
+++ include/depend.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+
+void pause() {
+    int x;
+    std::cin >> x;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,98 @@
 #include <iostream>
 #include <fstream>
+#include <main.hpp>
+#include <depend.cpp>
+
+void simulation() {
+    std::cout << "test";
+};
 
 int main()
 {
     int option = { 0 };
-    int location = { 0 };
+    int subMenu = { 0 };
 
     while (option != 5) {
         system("clear");
+
+        //logo
         std::cout << "  /$$$$$$                   /$$     /$$              \n /$$__  $$                 | $$    | $$              \n| $$  \\__/     /$$$$$$     | $$    | $$      /$$$$$$$\n| $$          /$$__  $$    | $$    | $$     /$$_____/\n| $$         | $$$$$$$$    | $$    | $$    |  $$$$$$ \n| $$    $$   | $$_____/    | $$    | $$     \\____  $$\n|  $$$$$$//$$|  $$$$$$$ /$$| $$ /$$| $$ /$$ /$$$$$$$/\n \\______/|__/ \\_______/|__/|__/|__/|__/|__/|_______/ \n                                                   ";
         std::cout << "\n" << "_________________________________________________________________\n \n";
 
+        //switch case handeling what page you are on
         switch (option)
         {
-        case 0:
-            std::cout << "1: Start simulation\n" << "2: Load memory\n" << "3: Eject memory\n" << "4: Settings\n" << "5: Exit\n";
-            std::cout << "\nType Option: ";
+        case 0: //main menu
+            std::cout << "1: Start\n" 
+            << "2: Load memory\n" 
+            << "3: Eject memory\n" 
+            << "4: Settings\n" 
+            << "5: Exit\n";
+            std::cout << "\n Type Option: ";
             std::cin >> option;
+            if(std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore();
+                option = 0;
+            }
             break;
 
-        case 1:
-            option = 0;
+        case 1: //starting simulation/game
+            std::cout << "1: Back\n" 
+            << "2: Simulation\n" 
+            << "3: Game\n";
+            std::cout << "\n Launch option: ";
+            std::cin >> subMenu;
+            if(std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore();
+                subMenu = 0;
+            }
+            if (subMenu == 1){
+                subMenu = 0;
+                option = 0;
+                break;
+            }
+            if (subMenu == 2){
+                simulation();
+                pause();
+            }
+            subMenu = 0;
             break;
 
         case 2:
-            std::cout << "1: back\n" << "2: localy\n";
+            std::cout << "1: Back\n" << "2: Localy\n";
             std::cout << "\n Load from: ";
-            std::cin >> location;
-            option = 0;
+            std::cin >> subMenu;
+            if(std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore();
+                subMenu = 0;
+            }
+            if(subMenu == 1) {
+                subMenu = 0;
+                option = 0;
+                break;
+            }
             break;
 
         case 3:
-            std::cout << "1: back\n" << "2: localy\n";
-            std::cout << "Store at: ";
-            std::cin >> location;
-            option = 0;
+            std::cout << "1: Back\n" << "2: Localy\n";
+            std::cout << "\n Store at: ";
+            std::cin >> subMenu;
+            if(std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore();
+                subMenu = 0;
+            }
+            if(subMenu == 1) {
+                subMenu = 0;
+                option = 0;
+                break;
+            }
             break;
 
-        case 4:
+        case 4: //settings
             option = 0;
             break;
 
